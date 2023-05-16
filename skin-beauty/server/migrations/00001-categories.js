f2e0676b6f75df04a62cf6ccc9ab63a7e9ef91bf
ecemkmz <ecemk259@gmail.com>
2023-05-16 16:10:25 +0300

    15.05.02

--- skin-beauty/server/migrations/00001-categories.js
+++ skin-beauty/server/migrations/00001-categories.js
@@ -2,7 +2,7 @@ Migrations.add({
   version: 1,
   name: "Created Category",
   up: function () {
-    const categories = JSON.parse(Assets.getText("/seeds/categories.json"));
+    const categories = JSON.parse(Assets.getText("seeds/categories.json"));
     categories.forEach((category) => {
       Category.insert(category);
     });
